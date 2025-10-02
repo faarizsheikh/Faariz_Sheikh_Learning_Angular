90de341f7364b97b50be16b5f31c918982653c38
faariz <faarizyt@gmail.com>
2025-10-02 18:54:16 -0400

    ADDED: CRUD (Create, Retrieve, Update, Delete) methods using the observable pattern.

--- src/app/services/game-data.ts
+++ src/app/services/game-data.ts
@@ -6,10 +6,37 @@ import {MOCK_CONTENT} from '../data/mock-content';
 @Injectable({
   providedIn: 'root'
 })
+
 export class GameDataService {
   private games = MOCK_CONTENT;
 
   getAll(): Observable<MyData[]> {
     return of(this.games);
+  }
+
+  getById(id: number): Observable<MyData | undefined> {
+    return of(this.games.find(game => game.id === id));
+  }
+
+  create(newGame: MyData): Observable<MyData[]> {
+    this.games.push(newGame);
+    return of(this.games);
+  }
+
+  update(updatedGame: MyData): Observable<MyData[]> {
+    const index = this.games.findIndex(game => game.id === updatedGame.id);
+    if (index > -1) {
+      this.games[index] = updatedGame;
+    }
+    return of(this.games);
+  }
+
+  delete(id: number): Observable<MyData | undefined> {
+    const index = this.games.findIndex(game => game.id === id);
+    if (index > -1) {
+      const removed = this.games.splice(index, 1)[0];
+      return of(removed);
+    }
+    return of(undefined);
   }
 }
