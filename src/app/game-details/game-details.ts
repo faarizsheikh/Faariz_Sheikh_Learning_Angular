dc493173d78b71c13d73bee7c167beaa8698f526
faariz <faarizyt@gmail.com>
2025-12-03 21:31:04 -0500

    V0.9.1: Minor improvements.
    🆕 ADDED:
    - Edit button in game-details.
    - Configuration: GitHub pages.
    https://faarizsheikh.github.io/Faariz_Sheikh_Learning_Angular
    
    🔁 CHANGED:
    - Material Tooltip appearance.

--- src/app/game-details/game-details.ts
+++ src/app/game-details/game-details.ts
@@ -1,16 +1,19 @@
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CustomTitlecasePipe } from '../pipes/custom-titlecase.pipe';
 import { GameDataService } from '../services/game-data-service';
 import { GameStatusColorPipe } from '../pipes/custom-game-status-colour.pipe';
+import { HoverHighlightDirective } from '../directives/hover-highlight.directive';
 import { MyData } from '../models/my-data';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 import { NormalizeSpacesPipe } from '../pipes/custom-normalize-spaces.pipe';
 
 @Component({
   selector: 'app-game-details',
   standalone: true,
-  imports: [CommonModule, CustomTitlecasePipe, GameStatusColorPipe, NormalizeSpacesPipe, RouterLink],
+  imports: [CommonModule, CustomTitlecasePipe, GameStatusColorPipe, MatIcon, MatTooltip, NormalizeSpacesPipe, RouterLink, HoverHighlightDirective],
   templateUrl: './game-details.html',
   styleUrls: ['./game-details.scss']
 })
@@ -19,7 +22,7 @@ export class GameDetails implements OnInit {
   game?: MyData | null;
   loading = true;
 
-  constructor(private route: ActivatedRoute, private gameService: GameDataService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private gameService: GameDataService) { }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -38,6 +41,10 @@ export class GameDetails implements OnInit {
       this.loading = false;
       this.game = null;
     }
+  }
+
+  editGame(game: MyData) {
+    this.router.navigate(['/modify', game.id]);
   }
 
   protected readonly isNaN = isNaN;
